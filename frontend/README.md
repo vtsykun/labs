6e020e31738631875e6031ddb3056fc039206a08
Jurasikt <tsykun314@gmail.com>
2016-03-05 20:51:41 +0300

    v 0.2.0

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,17 @@
 # Simple Chat
 
 You can see this chat [here](http://fpmi.physbox.info)
+
+## Full featured
+
+- simple interface
+- usability
+- base delete and edit your message
+- localstorage save
+- ajax load
+- xss and html injection protection
+- nginx logging
+
+# Licenses
+
+MIT License
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -16,12 +16,38 @@ get: function(cname) {
     }
 }
 
-function copy(author, message) {
+MessagesBox = {
+    maxId: 0,
+    items:{},
+};
+
+function printMessage(author, message, date, id) {
     var item = document.getElementsByClassName('message-box left none')[0].cloneNode(true);
     item.classList.remove('none');
-    item.getElementsByTagName('b')[0].innerText = '@'+author;
-    var inner = item.getElementsByClassName('message-text')[0];
-    inner.innerHTML = inner.innerHTML + message;
+    if (author == Cookie.get('nikename')) {
+        item.classList.add('left');
+        item.getElementsByTagName('b')[0].
+            setAttribute('tooltip-l',date+' UTC');
+    } else {
+        item.classList.add('right');
+        item.getElementsByTagName('b')[0].
+            setAttribute('tooltip-r',date+' UTC');
+    }
+    inner = item.getElementsByClassName('message-text')[0];
+    if (message == '') {
+        item.getElementsByClassName('action')[0].innerHTML = "";
+        inner.innerHTML = "Message has been was deleted";
+        inner.classList.remove('message-text');
+        inner.classList.add('delete');
+    } else {
+        item.getElementsByTagName('b')[0].innerText = '@'+author;
+        item.setAttribute("id", id);
+        item.getElementsByTagName('a')[0]
+            .setAttribute('onclick','editMessage('+id+')');
+        item.getElementsByTagName('a')[1]
+            .setAttribute('onclick','deleteMessage('+id+')');
+        inner.innerHTML = inner.innerHTML + htmlSpecialChars(message);        
+    }
     return item;
 }
 
@@ -31,19 +57,24 @@ function htmlSpecialChars(unsafe) {
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;")
-        .replace(/ /g, "&nbsp")
-        .replace(/'/g, "&#039;");
+        .replace(/'/g, "&#039;")
+        .replace(/\n/g, "<br>");
  }
 
 function sendMessage() {
     var input = document.getElementById('sendText');
-    if (input.value == '') {
-        alert('Сообщение не должно быть пустым');
+    if (input.value.replace(/ /g, '') == '') {
+        alert('Message can not be empty');
     } else {
-        item = copy(Cookie.get('nikename'), htmlSpecialChars(input.value));
+        date = simpleDate();
+        MessagesBox.items[(++MessagesBox.maxId)] = 
+            {datetime:date,text:input.value,"author":Cookie.get('nikename')};
+        item = printMessage(Cookie.get('nikename'), input.value, date, MessagesBox.maxId);
         input.value = "";
         console.log(item);
-        document.getElementsByClassName('scroll-container')[0].appendChild(item);        
+        scroll = document.getElementsByClassName('scroll-container')[0];
+        scroll.appendChild(item);
+        scroll.scrollTo(0,scroll.scrollHeight);
     }
 }
 
@@ -59,9 +90,104 @@ function modalTrigger(modal) {
     }
 }
 
+function simpleDate() {
+    s = new Date().toISOString();
+    return s.slice(0,10)+" "+s.slice(11,19);
+}
+
+function deleteMessage(id) {
+    if (confirm("Are you sure what you want to remove this message?")) {
+        MessagesBox.items[id].text = "";
+        item = document.getElementById(id);
+        inner = item.getElementsByClassName('message-text')[0];
+        inner.classList.add('delete');
+        inner.classList.remove('message-text');
+        inner.innerHTML = "Message has been was deleted";
+        item.getElementsByClassName('action')[0].innerHTML = "";        
+    }
+}
+
+function editMessage(id) {
+    form = document.getElementById('modal2');
+    form.getElementsByTagName('textarea')[0].value = MessagesBox.items[id].text;
+    form.getElementsByTagName('button')[0].
+        setAttribute('onclick','saveEditMessage('+id+')');
+    modalTrigger('modal2');
+}
+
+function saveEditMessage(id) {
+    now = simpleDate();
+    author = Cookie.get('nikename');
+    form = document.getElementById('modal2');
+    message = form.getElementsByTagName('textarea')[0].value;
+    if (message.replace(/ /g, '') == '') {
+        alert('Message can not be empty');
+    } else {
+        item = document.getElementById(id);
+        text = item.getElementsByClassName('message-text')[0];
+        text.innerHTML = '<b class="author"></b><br>' + htmlSpecialChars(message);
+        text.getElementsByTagName('b')[0].innerText = '@'+htmlSpecialChars(author);
+        item.getElementsByTagName('b')[0].
+            setAttribute('tooltip-l','Last-Modified: '+now+' UTC');
+        MessagesBox.items[id].text = message;
+        MessagesBox.items[id].date = now;
+        modalTrigger('modal2');        
+    }
+}
+
+
 window.onload = function(){
     document.getElementById('nikBtn').addEventListener('click', function(){
         Cookie.set('nikename', document.getElementById('nikInput').value);
         modalTrigger('modal1');
     });
+    document.getElementById("sendText").addEventListener("keydown", function(e) {
+        if (e.keyCode == 13 && e.shiftKey) {
+            sendMessage();
+        }
+    });
+    if (Cookie.get('nikename') == '') {
+        Cookie.set('nikename','Guest');
+    }
+    MessagesBox = localStorage.getItem('chat');
+    if (MessagesBox == null) {
+        MessagesBox = {
+            maxId: 0,
+            items:{},
+        };
+    } else {
+        MessagesBox = JSON.parse(MessagesBox);
+    }
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var json = JSON.parse(xmlhttp.responseText);
+            callback(json);
+        }
+    };
+    xmlhttp.open("GET", 'ajax/message.json', true);
+    xmlhttp.send();
+
+    function callback(json) {
+        scroll = document.getElementsByClassName('scroll-container')[0];
+        
+        for (var id in json) {
+            MessagesBox.items[id] = json[id];
+        }
+
+        for (var id in MessagesBox.items) {
+            item = MessagesBox.items[id];
+            inner = printMessage(item.author, item.text, item.datetime, id);
+            scroll.appendChild(inner);
+            if (MessagesBox.maxId < id) {
+                MessagesBox.maxId = id;
+            }
+        }
+        scroll.scrollTo(0,scroll.scrollHeight);
+    };
+}
+
+window.onbeforeunload = function(){
+    localStorage.setItem("chat", JSON.stringify(MessagesBox));
 }
